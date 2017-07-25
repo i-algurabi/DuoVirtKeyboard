05827f868a5592196937a0f761782f681688da5a
i.al-gurabi <i.al-gurabi@touchbank.com>
2017-07-25 17:59:55 +0300

    CSS optimizations

--- options.js
+++ options.js
@@ -1,49 +1,49 @@
 // Saves options to chrome.storage
 function save_options() {
-	settings.apply = document.getElementById('apply').checked;
-	settings.show = document.getElementById('show').checked;
-	saveToLocalStorage("settings", settings)
-	// Update status to let user know options were saved.
-	var status = document.getElementById('status');
-	status.textContent = 'Options saved.';
-	setTimeout(function () {
-		status.textContent = '';
-	}, 750);
+    settings.apply = document.getElementById('apply').checked;
+    settings.show = document.getElementById('show').checked;
+    saveToLocalStorage("settings", settings)
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function () {
+        status.textContent = '';
+    }, 750);
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-	// Use default value color = 'red' and likesColor = true.
-	var settings = getFromLocalStorage("settings");
-	document.getElementById('apply').checked = settings.apply;
-	document.getElementById('show').checked = settings.show;
+    // Use default value color = 'red' and likesColor = true.
+    var settings = getFromLocalStorage("settings");
+    document.getElementById('apply').checked = settings.apply;
+    document.getElementById('show').checked = settings.show;
 }
 
 function saveToLocalStorage(parameter, value) {
-	if (window.localStorage !== undefined) {
-		var localStorage = window.localStorage;
-		localStorage["keyboard." + parameter] = JSON.stringify(value);
-		return true;
-	}
-	return false;
+    if (window.localStorage !== undefined) {
+        var localStorage = window.localStorage;
+        localStorage["keyboard." + parameter] = JSON.stringify(value);
+        return true;
+    }
+    return false;
 }
 
 function getFromLocalStorage(parameter) {
-	if (window.localStorage !== undefined) {
-		var localStorage = window.localStorage;
-		var status = document.getElementById('status');
-		var str="";
-		for (z in localStorage){
-			str+=z + ":" + localStorage[z];
-		}
-		status.textContent = str;
-		var param = localStorage["keyboard." + parameter];
-		if (param)
-			console.info(param);
-			return JSON.parse(param);
-	}
-	return false;
+    if (window.localStorage !== undefined) {
+        var localStorage = window.localStorage;
+        var status = document.getElementById('status');
+        var str = "";
+        for (z in localStorage) {
+            str += z + ":" + localStorage[z];
+        }
+        status.textContent = str;
+        var param = localStorage["keyboard." + parameter];
+        if (param)
+            console.info(param);
+        return JSON.parse(param);
+    }
+    return false;
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
